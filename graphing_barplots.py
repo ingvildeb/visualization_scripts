c6306684864dcdfc59944fca4d70afcf90e8ff5a
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2025-07-31 18:01:12 -0400

    Documentation and generalizability
    Improved documentation, generalizability (plot densities, total numbers or volumes)

--- graphing_barplots.py
+++ graphing_barplots.py
@@ -2,9 +2,47 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
 import json
-from utils import prepare_hierarchy_info, add_bracket, create_child_to_parent_mapping, create_reverse_id_mapping, load_and_prepare_data
-from utils import collect_densities, create_barplot, average_density_dicts, normalize_density_values, create_heatmap
+from utils import prepare_hierarchy_info, create_child_to_parent_mapping, load_and_prepare_data
+from utils import collect_values, create_barplot, average_value_dicts
 
+"""
+
+BACKGROUND
+
+This script allows you to plot barplots for two or more groups. Bars will be color coded by the Allen atlas hierarchy, and shaded differently
+per group.
+
+You can choose which level of the Allen CCF hierarchy to plot the graphs at. The full hierarchy option will give you the finest granularity.
+Custom levels 1 through 7 are increasingly coarse levels. For this particular plot, Levels 4 through 7 are usually suitable if a parent is not
+specified. Finer levels should be plotted with a parent specified to avoid too crowded graphs. 
+
+Available hierarchy levels:
+
+        "FullHierarchy"
+        "CustomLevel1_gm"
+        "CustomLevel1_wm"
+        "CustomLevel2_gm"
+        "CustomLevel3_gm"
+        "CustomLevel4_gm"
+        "CustomLevel5_gm"
+        "CustomLevel6_gm"
+        "CustomLevel7_gm"
+
+The specified parent must be the name of a region at a coarser hierarchy level than the selected hierarchy, and this name must exist on the specified
+parent_hierarchy_level. The different hierarchy levels available can be found as excel files in the files folder of this repository. The default parent
+hierarchy level is Allen_STlevel_5, and this works well for most purposes. 
+
+Available grey matter parents at this level include:
+Isocortex, Olfactory areas, Hippocampal formation, Cortical subplate, Striatum, Pallidum, Thalamus, Hypothalamus, Midbrain, Pons, Medulla, Cerebellum
+
+Available white matter, ventricular and other parents at this level include:	
+cranial nerves, cerebellum related, fiber tracts, lateral forebrain bundle system, extrapyramidal fiber systems, medial forebrain bundle system,	
+ventricular systems, grooves, retina, supra-callosal cerebral white matter, fiber tracts & ventricular system
+
+If you want to plot very regions at the FullHierarchy level, you might want to choose a finer parent hierarchy level. You can always refer to the CustomLevel
+excel files to figure out which parents are available at which levels. 
+
+"""
 
 #### USER INPUTS
 files = [
@@ -16,14 +54,26 @@ files = [
     # Add more subject files as needed
 ]
 
-out_filename_prefix = "NeuN_P533_C57_"
+# Choose your hierarchy level and optionally a parent level (refer to the background section above for details)
+selected_hierarchy = "CustomLevel1_gm"
+specified_parent = "Isocortex" # Set to False if you want to plot data from the selected hierarchy level across the brain
+parent_hierarchy_level = "Allen_STlevel_5"
+
+# Choose the metric you want to plot. Use "cell_counted" for absolute numbers, "cell_value" for values and "ROI_Volume_mm_3" for region volumes
+value_column = "cell_counted"
+
+# Choose a prefix that will be added to your saved file name
+out_filename_prefix = "NeuN_18mo_C57_vs_14mo_TgSwDI--"
+
+# Choose the output format. tif is good for images to be used in presentation. svg is good if you want to further 
+# edit the figure, e.g. for using it in a publication figure or poster.
 out_format = "tif"
-plot_title = "NeuN cell densities"
-selected_hierarchy = "FullHierarchy"
-specified_parent = "Hippocampal formation"
+
+# Choose a title for your plot
+plot_title = "NeuN cell density"
+
 
 #### MAIN CODE, do not change
-
 
 # Path setup
 base_path = Path(__file__).parent.resolve()
@@ -32,44 +82,44 @@ hierarchy_file = base_path / "files" / "CCF_v3_ontology.json"
 custom_hier_path = base_path / "files"
 out_path = base_path / "plots"
 out_path.mkdir(parents=True, exist_ok=True)
-save_path = Path(out_path / f"{out_filename_prefix}_{selected_hierarchy}_{specified_parent}.{out_format}")
+save_path = Path(out_path / f"{out_filename_prefix}_{selected_hierarchy}_{specified_parent}_{value_column}.{out_format}")
 n = len(files)
 
-# Prepare density data
-all_densities = []
+# Prepare value data
+all_values = []
 
 for file in files:
     data_file = load_and_prepare_data(file, allen2intfile)
     id_mapping, color_mapping, acronym_mapping, hierarchy_regions = prepare_hierarchy_info(hierarchy_file, custom_hier_path)
-    child_to_parent_dict = create_child_to_parent_mapping(custom_hier_path, "Allen_STlevel_5")
+    child_to_parent_dict = create_child_to_parent_mapping(custom_hier_path, parent_hierarchy_level)
 
-    # Collect the densities and corresponding region names, colors and parent regions
+    # Collect the values and corresponding region names, colors and parent regions
     
-    densities_in_file = collect_densities(data_file, hierarchy_regions, selected_hierarchy, child_to_parent_dict, specified_parent)
-    all_densities.append(densities_in_file)
+    values_in_file = collect_values(data_file, value_column, hierarchy_regions, selected_hierarchy, child_to_parent_dict, specified_parent)
+    all_values.append(values_in_file)
 
-# Get average densities and SEM if n>1
+# Get average values and SEM if n>1
 if n > 1:
-    density_dict, se_dict = average_density_dicts(all_densities)
+    value_dict, se_dict = average_value_dicts(all_values)
 else:
-    density_dict, se_dict = all_densities[0], None
+    value_dict, se_dict = all_values[0], None
 
 
 # Prepare lists for plotting
-densities = []
+values = []
 standard_errors = []
 region_names = []
 region_colors = []
 parent_labels = []
 
-for key, value in density_dict.items():
+for key, value in value_dict.items():
     region_id = key
-    density = value
+    value = value
     region_name = id_mapping.get(region_id)
     region_color = f"#{color_mapping.get(region_id)}"
     parent_region = child_to_parent_dict.get(region_id)
     
-    densities.append(density)
+    values.append(value)
     region_names.append(region_name)
     region_colors.append(region_color)
     parent_labels.append(parent_region)
@@ -84,6 +134,6 @@ for i in range(len(parent_labels) - 1):
         parent_labels[i] = parent_labels[i + 1]
 
 if n > 1:
-    create_barplot(save_path, region_names, densities, region_colors, parent_labels, plot_title, yerr=standard_errors, selected_hierarchy = selected_hierarchy, specified_parent = specified_parent)
+    create_barplot(save_path, region_names, values, region_colors, parent_labels, plot_title, yerr=standard_errors, selected_hierarchy=selected_hierarchy, specified_parent=specified_parent)
 else:
-    create_barplot(save_path, region_names, densities, region_colors, parent_labels, plot_title, selected_hierarchy = selected_hierarchy, specified_parent = specified_parent)
+    create_barplot(save_path, region_names, values, region_colors, parent_labels, plot_title, selected_hierarchy=selected_hierarchy, specified_parent=specified_parent)
